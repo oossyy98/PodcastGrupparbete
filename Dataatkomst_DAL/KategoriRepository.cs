426033651eaf40081d8f57814a601c20c955c3da
felipecigel <felipecigel@outlook.com>
2025-11-11 14:11:30 +0100

    fixat kategori

--- Dataatkomst_DAL/KategoriRepository.cs
+++ Dataatkomst_DAL/KategoriRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Models;
+using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,39 @@ using System.Threading.Tasks;
 
 namespace DAL_DataAccess
 {
-    internal class KategoriRepository
+    public class KategoriRepository : IRepository<Kategori>
     {
+        private readonly IMongoCollection<Kategori> _kollektion; //MongoDB collection for Kategori
+        private readonly MongoDBConnection _connection;
+
+        public KategoriRepository(MongoDBConnection anslutning)
+        {
+            _connection = anslutning;
+            _kollektion = anslutning.HamtaKollektion<Kategori>("kategorier");
+        }
+        public async Task Create(Kategori item)
+        {
+            await _kollektion.InsertOneAsync(item);
+        }
+
+        public Task Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Kategori>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Kategori> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(string id, Kategori item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
