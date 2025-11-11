62160d8528f3b1286a7bcab2c9a04ec51703648a
oossyy98 <125208207+oossyy98@users.noreply.github.com>
2025-11-11 19:43:06 +0100

    Update IPodcastService.cs
    IPodcastService klar

--- BL_BusinessLogic/IPodcastService.cs
+++ BL_BusinessLogic/IPodcastService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace BL_BusinessLogic
 {
-    internal interface IPodcastService
+    public interface IPodcastService
     {
+        //CREATE
+        Task LaggTill(Podcast podcast);
+
+        //READ
+        Task<List<Podcast>> HamtaAlla();
+        Task<Podcast> HamtaMedId(string id);
+
+        //UPDATE
+        Task<bool> Uppdatera(string id, Podcast podcast);
+
+        //DELETE
+        Task<bool> TaBort(string id);
     }
 }
