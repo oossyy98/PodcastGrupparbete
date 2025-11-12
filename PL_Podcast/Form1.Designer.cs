8c7e1161616e713412b1d62e8f29bf7df62f2152
Oliverleallees <oliverlees2001@gmail.com>
2025-11-12 16:48:21 +0100

    test
    design

--- PL_Podcast/Form1.Designer.cs
+++ PL_Podcast/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblNamn = new Label();
+            SuspendLayout();
+            // 
+            // lblNamn
+            // 
+            lblNamn.AutoSize = true;
+            lblNamn.Location = new Point(22, 21);
+            lblNamn.Name = "lblNamn";
+            lblNamn.Size = new Size(79, 32);
+            lblNamn.TabIndex = 0;
+            lblNamn.Text = "Namn";
+            lblNamn.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblNamn);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNamn;
     }
 }
--- PL_Podcast/Form1.cs
+++ PL_Podcast/Form1.cs
@@ -6,5 +6,15 @@ namespace PL_Podcast
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
